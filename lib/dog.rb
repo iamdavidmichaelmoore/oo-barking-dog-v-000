aded0e660367ba094b010eca49d1d49876ef9374
David Moore <hey@iamdavidmoore.com>
2018-04-13 18:34:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+
+  def initializes(name)
+    @name = new
+  end
+
+  #getter methods
+  def name
+    @name
+  end
+
+  #attribute methods
+  def bark
+    puts "Woof!"
+  end
+end
+
+  
